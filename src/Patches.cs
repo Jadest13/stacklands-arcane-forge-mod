fec07bd91d61bd261e1e9b6611313d79bf8eefca
Jadest13 <jadest13x03@gmail.com>
2026-01-09 03:37:02 +0900

    add Chinese(Simplified)

--- src/Patches.cs
+++ src/Patches.cs
@@ -1,4 +1,6 @@
 ﻿using HarmonyLib;
+using System;
+using UnityEngine;
 
 namespace ArcaneForgeNS
 {
@@ -29,5 +31,47 @@ namespace ArcaneForgeNS
             bool isMine = baseId == "gravel_pit" || baseId == "copper_mine" || baseId == "uranium_mine";
             __result = __result || (isMine && otherCard.Id == Consts.Card(Consts.ORE_EXCAVATOR));
         }
+
+        //[HarmonyPatch(typeof(SokLoc), "LoadTermsFromFile")]
+        //[HarmonyPostfix]
+        //public static void Tmp(SokLoc __instance, string path, bool disableWarning = false)
+        //{
+        //    if (!path.Contains("ArcaneForge")) return;
+        //    string[][] array = SokLoc.ParseTableFromTsv(File.ReadAllText(path));
+        //    int languageColumnIndex = SokLoc.GetLanguageColumnIndex(array, __instance.CurrentLanguage);
+        //    Debug.Log(path + ", " + languageColumnIndex);
+        //    if (languageColumnIndex == -1)
+        //    {
+        //        return;
+        //    }
+        //    for (int i = 1; i < array.Length; i++)
+        //    {
+        //        string term = array[i][0];
+        //        Debug.Log(i + ", " + term);
+        //        string fullText = array[i][languageColumnIndex];
+        //        term = term.Trim().ToLower();
+        //        if (!string.IsNullOrEmpty(term))
+        //        {
+        //            SokTerm sokTerm = new SokTerm(__instance.CurrentLocSet, term, fullText);
+        //            Debug.Log(sokTerm.GetText());
+        //            if (__instance.CurrentLocSet.TermLookup.ContainsKey(term))
+        //            {
+        //                if (!disableWarning)
+        //                {
+        //                    Debug.LogError("Term " + term + " has been found more than once in the localisation sheet. Using last item in sheet.");
+        //                }
+        //                __instance.CurrentLocSet.TermLookup[term] = sokTerm;
+        //                __instance.CurrentLocSet.AllTerms.RemoveAll((SokTerm x) => x.Id == term);
+        //                __instance.CurrentLocSet.AllTerms.Add(sokTerm);
+        //            }
+        //            else
+        //            {
+        //                __instance.CurrentLocSet.AllTerms.Add(sokTerm);
+        //                __instance.CurrentLocSet.TermLookup.Add(term, sokTerm);
+        //            }
+        //            Debug.Log(__instance.CurrentLocSet.TermLookup[term] + ", " + __instance.CurrentLocSet.TermLookup.TryGetValue(term, out var value));
+        //        }
+        //    }
+        //}
     }
 }
